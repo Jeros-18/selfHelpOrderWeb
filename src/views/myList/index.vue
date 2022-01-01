a3866321faf41b97eaea27caf1037ab70f495947
JiaHui <1474352803@qq.com>
2022-01-01 20:03:33 +0800

    订单页功能实现

--- src/views/myList/index.vue
+++ src/views/myList/index.vue
@@ -20,11 +20,12 @@
       <el-table-column prop="price" label="价格(/元)" width="130"/>
 
         <el-table-column prop="num" label="份量(/份)" width="100"/>
-        <el-table-column prop="(num*price)" label="总价"/>
+        
 
         
     </el-table>
-  合计：
+  合计：{{this.sum}}元
+  <el-button type="danger">确定支付</el-button>
   </div>
 </template>
 <script>
@@ -33,15 +34,19 @@ import food from "@/api/food";
 export default {
   data() {
     return {
-      b:2,
-      pick:0,
+  
       current: 1, //页码
       limit: 10, //每页多少行
       foodQuery: {}, //查询条件
       list: [], //列表数据
       total: 0, //总记录数
-      np:0
+      sum:0
     };
+  },
+  computed:{
+      sum(){
+          this.c=a*b;
+      }
   },
   created() {
     this.showDingd();
@@ -52,63 +57,13 @@ export default {
         food.showDing()
         .then(response=>{
             this.list=response.data.foodPickedList
+            for(let i=0;i<this.list.length;i++){
+                this.sum+=(this.list[i].price)*(this.list[i].num)
+            }
         })
     },
     
-    //加入订单
-    addOrderForm(id) {
-      this.$confirm("确定加入此菜品进入订单吗, 是否继续?", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning"
-      })
-        .then(() => {
-          food.addDing(id).then(response => {
-            //添加成功
-            console.log("添加成功");
-            this.$message({
-              type: "success",
-              message: "添加成功!"
-            });
-             //刷新表格
-            this.getFoodPageQuery();
-          });
-        })
-        .catch(() => {
-          this.$message({
-            type: "info",
-            message: "已取消添加"
-          });
-        });
-    },
-
-    //移出订单
-    removeOrderForm(id) {
-      this.$confirm("确定将此菜品移出订单吗, 是否继续?", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning"
-      })
-        .then(() => {
-          food.removeDing(id).then(response => {
-            //移出成功
-            console.log("移出成功");
-            this.$message({
-              type: "success",
-              message: "移出成功!"
-            });
-             //刷新表格
-            this.getFoodPageQuery();
-          });
-        })
-        .catch(() => {
-          this.$message({
-            type: "info",
-            message: "已取消移出"
-          });
-        });
-    },
-
+   
     
   }
 };
